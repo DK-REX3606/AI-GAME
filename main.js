7b4fb4199270c357370473db2d8c1344f2308db0
DK-REX3606 <71270037+DK-REX3606@users.noreply.github.com>
2020-10-23 18:34:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function preload() {
-	mario_game_over = loadSound("gameover.wav");
+	mario_gameover = loadSound("gameover.wav");
 	mario_jump = loadSound("jump.wav");
 	mario_coin = loadSound("coin.wav");
 	mario_kick = loadSound("kick.wav");
@@ -8,26 +8,36 @@ function preload() {
 	setSprites();
 	MarioAnimation();
 }
+
 function setup() {
-	canvas = createCanvas(1240, 336);
+	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
+
 	instializeInSetup(mario);
+	
 	video = createCapture(VIDEO);
-	video.size(800, 400);
+	video.size(800,400);
 	video.parent('game_console');
+
 	poseNet = ml5.poseNet(video, modelLoaded);
 	poseNet.on('pose', gotPoses);
+
 }
-function modelLoaded(){
-	console.log("Pose Net is intialized");
-}
+
+function modelLoaded() {
+	console.log('Model Loaded!');
+  }
+
+  function gotPoses(results)
+  {
+	if(results.length > 0)
+	{
+	  console.log(results);
+	  noseX = results[0].pose.nose.x;
+	  noseY = results[0].pose.nose.y;
+	}
+  }
+   
 function draw() {
 	game();
-}
-function gotPoses(results) { 
-	if(results.length > 0) {
-		 console.log(results); 
-		 noseX = results[0].pose.nose.x; 
-		 noseY = results[0].pose.nose.y; 
-		} 
 }
